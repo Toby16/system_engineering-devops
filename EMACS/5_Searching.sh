05e497b0d2048dab294754afceb1e929ba75d489
Baasit Bolaji Ayomiposi <bolajibaasit14@gmail.com>
2025-10-06 17:44:03 +0100

    Create 5_Searching.sh

---
+++ EMACS/5_Searching.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# The command to search forward
+# Write the answer into the file /root/0x01_emacs/searching.
+
+mkdir -p 0x01_emacs
+echo "C-s" > /root/0x01_emacs/searching
+ls -l /root/0x01_emacs/searching
