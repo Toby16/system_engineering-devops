46ebaccc3d37065e5afc9b8b960bd0d08a6f5d22
Baasit Bolaji Ayomiposi <bolajibaasit14@gmail.com>
2025-10-06 17:27:58 +0100

    Create 2_Saving.sh

---
+++ EMACS/2_Saving.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# The command to save a file?
+# Write the answer into the file /root/0x01_emacs/saving.
+
+mkdir -p 0x01_emacs
+echo "C-x C-s" > /root/0x01_emacs/saving
+ls -l /root/0x01_emacs/saving
