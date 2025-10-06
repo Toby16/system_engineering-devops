9b01f7dc4761355a5cdc9d771bce5a2dd5f982d1
Baasit Bolaji Ayomiposi <bolajibaasit14@gmail.com>
2025-10-06 17:57:00 +0100

    Create 7_Quitting.sh

---
+++ EMACS/7_Quitting.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# The command to quit Emac
+# Write the answer into the file /root/0x01_emacs/quitting.
+
+mkdir -p 0x01_emacs
+echo "C-x C-c" > /root/0x01_emacs/quitting
+ls -l /root/0x01_emacs/quitting
