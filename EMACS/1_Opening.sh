eec30167046a5bb053b4343183b0611c14aea070
Baasit Bolaji Ayomiposi <bolajibaasit14@gmail.com>
2025-10-06 17:17:54 +0100

    Create 1_Opening.sh

---
+++ EMACS/1_Opening.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# What is the command to open a file from within Emacs?
+# Write the answer into the file /root/0x01_emacs/opening.
+# You can validate if the format of your answer is correct by displaying the file information
+
+mkdir -p 0x01_emacs
+echo "C-x C-f" > /root/0x01_emacs/opening
+ls -l /root/0x01_emacs/opening
