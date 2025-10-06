07055078765ded39746b460fce744aa8250f5d2b
Baasit Bolaji Ayomiposi <bolajibaasit14@gmail.com>
2025-10-06 17:41:38 +0100

    Create 4_Pasting.sh

---
+++ EMACS/4_Pasting.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# The command to paste in Emacs.
+# Write the answer into the file /root/0x01_emacs/pasting.
+
+mkdir -p 0x01_emacs
+echo "C-y" > /root/0x01_emacs/pasting
+ls -l /root/0x01_emacs/pasting
