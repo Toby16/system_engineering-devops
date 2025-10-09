937e6d8a0eb7e6888a9f728aa22be77e1d3f56c7
Baasit Bolaji Ayomiposi <bolajibaasit14@gmail.com>
2025-10-09 12:14:53 +0100

    Create 1_More_of_me.sh

---
+++ SHELL_NAVIGATION/1_More_of_me.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env activate
+# Change your working directory to /root
+# Then, copy the file `school` to /tmp
+
+cd /root
+cp school /tmp
+
+# Validate your current working directory
+pwd
+# Display the list of files of your current directory to validate the creation of the new file
+ls -l . /tmp # displays content of current directory and /tmp in long format
