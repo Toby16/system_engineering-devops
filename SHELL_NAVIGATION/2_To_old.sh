7b840876957016344b473552f3e5574d7995822e
Baasit Bolaji Ayomiposi <bolajibaasit14@gmail.com>
2025-10-09 12:17:27 +0100

    Create 2_To_old.sh

---
+++ SHELL_NAVIGATION/2_To_old.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Change your working directory to /root
+# Then, rename the file `old_school` to `new_school` (in the same directory)
+
+cd /root
+mv old_school new_school
+
+# Validate your current working directory
+pwd
+# Display the list of files of your current directory to validate the creation of the new file
+ls -l # displays in the long format
