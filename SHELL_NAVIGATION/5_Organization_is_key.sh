3343d17fb8fd037dc4e86fbce53dd3bb7749f27a
Baasit Bolaji Ayomiposi <bolajibaasit14@gmail.com>
2025-10-09 12:50:56 +0100

    Create 5_Organization_is_key.sh

---
+++ SHELL_NAVIGATION/5_Organization_is_key.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Change your working directory to /root
+# Then, create a directory `school_is_amazing`
+
+cd /root
+mkdir school_is_amazing
+
+# Validate your current working directory
+pwd
+# Display the list of files of your current directory to validate the creation of the directory
+ls -l # displays in the long format
