6e997774e11ad1d3b87a02b1ac539db979ef639a
Baasit Bolaji Ayomiposi <bolajibaasit14@gmail.com>
2025-10-09 06:06:13 +0100

    Create 0_Create_me.sh

---
+++ SHELL_NAVIGATION/0_Create_me.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env  bash
+# Change your working directory to /root
+# Then, create an empty file so_cool
+
+cd /root
+touch so_cool
+
+# Validate your current working directory
+pwd
+# Display the list of files of your current directory to validate the creation of the new file
+ls -l # displays in the long format
