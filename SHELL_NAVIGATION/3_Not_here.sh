0a45b8232b54420baa7088011d585c2e4c3950cb
Baasit Bolaji Ayomiposi <bolajibaasit14@gmail.com>
2025-10-09 12:39:44 +0100

    Create 3_Not_here.sh

---
+++ SHELL_NAVIGATION/3_Not_here.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Change your working directory to /root
+# Then, move the file `not_here` to /tmp/right_school
+
+cd /root
+rm -rf not_here && touch not_here  # To ensure not_here is an empty file | A Requirement!
+mv not_here /tmp/right_school  
+
+# Validate your current working directory
+pwd
+# Display the list of files of your current directory to validate the creation of the new file
+ls -l . /tmp # displays content of current directory and /tmp in long format
