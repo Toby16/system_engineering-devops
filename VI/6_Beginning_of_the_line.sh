5e3867f1e105f6fe66986ecdb5d01aa537f7e1c5
Toby 👹 <bolajibaasit14@gmail.com>
2025-10-14 21:08:11 +0100

    correct answer updated along with commands for automation

--- VI/6_Beginning_of_the_line.sh
+++ VI/6_Beginning_of_the_line.sh
@@ -1,1 +1,10 @@
-0
+#!/usr/bin/env bash
+# The command to move the cursor to the start of the current line
+# Write the answer into the file /root/0x02_vi/beginning_of_the_line.
+
+cd /root
+mkdir -p 0x02_vi  # -p ensures no error if it already exists (idempotent)
+echo "0" > 0x02_vi/beginning_of_the_line  # the command is "0"
+
+# Validate if the format of your answer is correct by displaying the file information
+ls -l 0x02_vi/beginning_of_the_line
